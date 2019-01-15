ee7d0d72643a1753211165cb8ce00bd12d0d5c38
Paul Jickling <pjickling@gmail.com>
2019-01-14 17:21:58 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# minigrep
+
+A simple command line interface for a grep-esque tool built in Rust as covered in chapter 12 of the Rust book.
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+   let config = parse_config(&args); 
+
+    println!("Searching for: {}", config.query);
+    println!("In file: {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename).expect("Error reading file");
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
+}
